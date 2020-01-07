e0b1cd1404d0233efb1a66ee6ac7c407e91c0845
Ben Blumenfeld <ben.blumenfeld@myagi.com>
2020-01-07 15:32:47 -0800

    finished and deployed

--- mc_updater.py
+++ mc_updater.py
@@ -1,16 +1,20 @@
-import requests
-from twilio.rest import Client
+import boto3
 import config
 import os
+import requests
+from twilio.rest import Client
 
-#set as env variables lambda console
+
+# set as env variables lambda console
 MISSION_CLIFFS_ENDPONT = os.getenv('MISSION_CLIFFS_ENDPONT')
-TWILIO_SID =  os.getenv('TWILIO_SID')
-TWILIO_AUTH_TOKEN =  os.getenv('TWILIO_AUTH_TOKEN')
-TWILIO_TRIAL_NUMBER =  os.getenv('TWILIO_TRIAL_NUMBER')
-#list of numbers that will recieve text
+TWILIO_SID = os.getenv('TWILIO_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+TWILIO_TRIAL_NUMBER = os.getenv('TWILIO_TRIAL_NUMBER')
+# list of numbers that will recieve text
 NUMBERS = config.NUMBERS
-MOST_RECENT_POST_PATH = './most_recent_post.txt'
+BUCKET = os.getenv('BUCKET')
+UUID_OBJECT_KEY = os.getenv('UUID_OBJECT_KEY')
+S3 = boto3.resource('s3')
 NEW_ROUTE_KEYWORDS = [
 
     "new",
@@ -25,13 +29,13 @@ NEW_ROUTE_KEYWORDS = [
 
 
 def is_new_route_match(caption):
-    caption  = caption.lower()
+    caption = caption.lower()
     is_possible_new_route = False
     for keyword in NEW_ROUTE_KEYWORDS:
         if caption.find(keyword) > 0:
             is_possible_new_route = True
     return is_possible_new_route
-    
+
 
 def get_mc_most_recent_post_caption_text(mc_profile_html_text):
     post_short_code = get_mc_most_recent_post_uuid(mc_profile_html_text)
@@ -54,26 +58,31 @@ def get_mc_most_recent_post_uuid(mc_profile_html_text):
     return mc_profile_html_text[short_code_start_index:short_code_end_index]
 
 
-def update_local_post_uuid(new_uuid):
-    most_recent_post_file = open(MOST_RECENT_POST_PATH, "w")
-    most_recent_post_file.write(new_uuid)
-    most_recent_post_file.close()
+def update_last_uuid(new_uuid, s3_object):
+    """
+    Updates s3 last checked uuid object
+    """
+    encoded_uuid = new_uuid.encode()
+    s3_object.put(Body=encoded_uuid)
 
 
-def get_local_post_uuid():
-    most_recent_post_file = open(MOST_RECENT_POST_PATH, "r")
-    uuid = most_recent_post_file.read()
-    most_recent_post_file.close()
+def get_last_uuid(s3_object):
+    """
+    Reaches out to s3 to get last uuid checked by this function.
+    """
+    s3_object_body = s3_object.get()["Body"]
+    uuid = s3_object_body.read().decode('utf-8')
     return uuid
 
 
 def main():
     mc_profile_html_text = requests.get(MISSION_CLIFFS_ENDPONT).text
     most_recent_post_uuid = get_mc_most_recent_post_uuid(mc_profile_html_text)
-    local_uuid = get_local_post_uuid()
-    if most_recent_post_uuid == local_uuid:
+    most_recent_s3_object = S3.Object(BUCKET, UUID_OBJECT_KEY)
+    last_uuid = get_last_uuid(most_recent_s3_object)
+    if most_recent_post_uuid == last_uuid:
         return
-    update_local_post_uuid(most_recent_post_uuid)
+    update_last_uuid(most_recent_post_uuid, most_recent_s3_object)
     most_recent_caption = get_mc_most_recent_post_caption_text(mc_profile_html_text)
     if is_new_route_match(most_recent_caption):
         for number in NUMBERS:
